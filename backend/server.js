bac1187875626861c395723bdcbb9a19d26c5c9a
Auksea <66auksyte@gmail.com>
2023-05-26 15:03:14 +0200

    Latest version of ETL process and added delete buttons

--- backend/server.js
+++ backend/server.js
@@ -27,89 +27,6 @@ console.log('Connection to MySQL is successful');
 // Create a connection string to connect to your Azure Blob Storage
 const connectionString = 'DefaultEndpointsProtocol=https;AccountName=csvfromocutune;AccountKey=M5zsYcaXDYmpo8xF+QxiSOlkBM6p7os4t7i/b89tOZEEowNbBTH41LSdWYohzCYjue6FIjRQ01WZ+AStM+2y0Q==;EndpointSuffix=core.windows.net';
 
-app.get('/etl', async (req, res) => {
-  console.log('ETL endpoint called');
-  try {
-    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-
-    // Get a reference to the container in your Azure Blob Storage
-    const containerName = 'csvfiles';
-    const containerClient = blobServiceClient.getContainerClient(containerName);
-
-    // Define the name of the Excel file you want to process
-    const fileName = 'SpectrumData_2023-05-22T104654.612995.csv';
-
-    // Get a reference to the blob (Excel file)
-    const blobClient = containerClient.getBlobClient(fileName);
-
-    // Download the Excel file as a buffer
-    const downloadResponse = await blobClient.download();
-    const buffer = await streamToBuffer(downloadResponse.readableStreamBody);
-
-    // Parse the Excel file using PapaParse
-    const csvData = Papa.parse(Buffer.from(buffer).toString(), { header: true }).data;
-    console.log('csvData:', csvData);
-
-    const mapping = {
-      'Photopic Lux': 'LUX',
-      'CCT': 'CCT',
-      'Erythropic Lux': 'mEDI',
-      'Melanopic Lux': 'GIndex_LightPollution',
-      'PPFD-G': 'Rodent_Mlux'
-    };
-
-    const concatenatedValues = {};
-
-    for (const row of csvData) {
-      const fileName = row['File Name'];
-      const value = parseFloat(row[' test']);
-
-      if (!isNaN(value) && fileName in mapping) {
-        const column = mapping[fileName];
-
-        if (concatenatedValues[column]) {
-          concatenatedValues[column] += `,${value}`;
-        } else {
-          concatenatedValues[column] = String(value);
-        }
-      }
-    }
-
-    console.log('Concatenated Values:', concatenatedValues);
-
-    // Insert the concatenated values into the MySQL database
-    try {
-      const queryValues = Object.values(concatenatedValues);
-
-      if (queryValues.length > 0) {
-        const query = `INSERT INTO measurements (${Object.keys(concatenatedValues).join(',')}) VALUES (${queryValues.map(() => '?').join(',')})`;
-
-        console.log('SQL Query:', query);
-        console.log('Query Values:', queryValues);
-
-        await executeQuery(query, queryValues);
-
-        console.log('Inserted rows:', csvData.length);
-      } else {
-        console.log('No rows to insert');
-      }
-    } catch (error) {
-      console.error('Error inserting rows:', error);
-    }
-
-    res.json({ message: 'ETL process completed successfully' });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred during the ETL process' });
-  }
-
-  console.log('ETL process completed');
-});
-
-
-
-
 // Function to execute a MySQL query
 function executeQuery(query, values) {
   return new Promise((resolve, reject) => {
@@ -133,15 +50,170 @@ function streamToBuffer(stream) {
   });
 }
 
-app.get('/measurements', (req, res) => {
-  connection.query('SELECT * FROM ocutunedb.measurements', (err, results) => {
-    if (err) throw err;
-    res.json(results);
+function isFileProcessed(fileName) {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT COUNT(*) AS count FROM processed_files WHERE file_name = ?';
+    connection.query(query, [fileName], (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        const count = results[0].count;
+        resolve(count > 0);
+      }
+    });
   });
+}
+
+
+function updateProcessedFile(fileName) {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO processed_files (file_name) VALUES (?)';
+    connection.query(query, [fileName], (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+
+
+
+async function performETL() {
+  console.log('ETL process started');
+  try {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+
+    // Get a reference to the container in your Azure Blob Storage
+    const containerName = 'csvfiles';
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // List all blobs in the container
+    const blobList = containerClient.listBlobsFlat();
+
+    // Get the latest uploaded CSV file
+    let latestBlob = null;
+
+    for await (const blob of blobList) {
+      const blobProperties = await containerClient.getBlobClient(blob.name).getProperties();
+      const fileName = blob.name;
+
+      if (!latestBlob) {
+        latestBlob = blob;
+      } else {
+        // Compare file names to get the latest file
+        if (fileName > latestBlob.name) {
+          latestBlob = blob;
+        }
+      }
+    }
+
+    if (!latestBlob) {
+      // No CSV file found
+      console.log('No CSV files found in the container');
+      return;
+    }
+
+    // Get the name of the latest CSV file
+    const fileName = latestBlob.name;
+    console.log('Latest CSV file:', fileName);
+
+    // Check if the file has already been processed
+    const isProcessed = await checkIfFileProcessed(fileName);
+
+    if (isProcessed) {
+      console.log('File has already been processed');
+      return;
+    }
+
+    // Get a reference to the blob (CSV file)
+    const blobClient = containerClient.getBlobClient(fileName);
+
+    // Download the Excel file as a buffer
+    const downloadResponse = await blobClient.download();
+    const buffer = await streamToBuffer(downloadResponse.readableStreamBody);
+
+    // Parse the Excel file using PapaParse
+    const csvData = Papa.parse(Buffer.from(buffer).toString(), { header: true }).data;
+    console.log('csvData:', csvData);
+
+    const mapping = {
+      'Photopic Lux': 'LUX',
+      'CCT': 'CCT',
+      'Erythropic Lux': 'mEDI',
+      'Melanopic Lux': 'GIndex_LightPollution',
+      'PPFD-G': 'Rodent_Mlux'
+    };
+
+    const concatenatedValues = {};
+
+    // Insert the concatenated values into the MySQL database
+    try {
+      const queryValues = Object.values(concatenatedValues);
+
+      if (queryValues.length > 0) {
+        const query = `INSERT INTO measurements (${Object.keys(concatenatedValues).join(',')}) VALUES (${queryValues.map(() => '?').join(',')})`;
+
+        console.log('SQL Query:', query);
+        console.log('Query Values:', queryValues);
+
+        await updateProcessedFile(latestFileName);
+        await executeQuery(query, queryValues);
+
+
+        console.log('Inserted rows:', csvData.length);
+      } else {
+        console.log('No rows to insert');
+      }
+
+      await updateProcessedFile(fileName);
+    } catch (error) {
+      console.error('Error inserting rows:', error);
+      return;
+    }
+
+    console.log('ETL process completed');
+  } catch (error) {
+    console.error('ETL process failed:', error);
+  }
+}
+
+app.get('/measurements', async (req, res) => {
+  try {
+    // Only perform ETL process if there is a new CSV file uploaded
+    await performETL();
+
+    connection.query('SELECT * FROM ocutunedb.measurements', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  } catch (error) {
+    console.error('ETL process failed:', error);
+    res.status(500).json({ error: 'An error occurred during the ETL process' });
+  }
 });
 
+app.delete('/measurements/:serialNo', async (req, res) => {
+  const serialNo = req.params.serialNo;
 
+  try {
+    // Delete the row with the specified SerialNo
+    await executeQuery('DELETE FROM measurements WHERE SerialNo = ?', [serialNo]);
 
+    // Delete the dataset associated with the SerialNo (assuming there is a corresponding file in the Azure Blob Storage)
+    // Write the code to delete the dataset from Azure Blob Storage here
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+    // Return a success message or any other desired response
+    res.json({ message: 'Record and dataset deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting record:', error);
+    res.status(500).json({ error: 'An error occurred while deleting the record and dataset' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
 
--- src/Welcome.js
+++ src/Welcome.js
@@ -17,32 +17,46 @@ function Welcome() {
     }
   }
 
+  async function handleDelete(serialNo) {
+    try {
+      await axios.delete(`http://localhost:3000/measurements/${serialNo}`);
+      fetchData(); // Fetch the updated data after successful deletion
+    } catch (error) {
+      console.error(error);
+    }
+  }  
+
   return (
     <div>
       <h1>Welcome</h1>
       <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-        <thead>
-          <tr>
-            <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>Serial No</th>
-            <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>LUX</th>
-            <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>CCT</th>
-            <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>mEDI</th>
-            <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>GIndex(Light Pollution)</th>
-            <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>Rodent(m-lux)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.SerialNo}>
-              <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.SerialNo}</td>
-              <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.LUX}</td>
-              <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.CCT}</td>
-              <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.mEDI}</td>
-              <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.GIndex_LightPollution}</td>
-              <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.Rodent_Mlux}</td>
-            </tr>
-          ))}
-        </tbody>
+      <thead>
+      <tr>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>Serial No</th>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>LUX</th>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>CCT</th>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>mEDI</th>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>GIndex(Light Pollution)</th>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>Rodent(m-lux)</th>
+        <th style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>Actions</th> {/* Added delete button header */}
+      </tr>
+    </thead>
+
+    <tbody>
+      {data.map(item => (
+        <tr key={item.SerialNo}>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.SerialNo}</td>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.LUX}</td>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.CCT}</td>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.mEDI}</td>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.GIndex_LightPollution}</td>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>{item.Rodent_Mlux}</td>
+          <td style={{ textAlign: 'center', padding: '8px', border: '1px solid black' }}>
+            <button onClick={() => handleDelete(item.SerialNo)}>Delete</button>
+          </td> {/* Added delete button */}
+        </tr>
+      ))}
+    </tbody>
       </table>
     </div>
   );
